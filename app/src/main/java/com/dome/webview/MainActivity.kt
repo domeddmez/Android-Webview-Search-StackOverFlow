19016639297c62215264da87311b9cdbd6e8b3e8
dome <cat333825@gmail.com>
2019-07-24 16:16:22 +0700

    no message

---
+++ app/src/main/java/com/dome/webview/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.dome.webview
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            hideSoftKeyboard(this, it)
+            webView.loadUrl("https://stackoverflow.com/search?q=%5Bandroid%5D ${editText.text}")
+        }
+    }
+
+    private fun hideSoftKeyboard(activity: Activity, view: View) {
+        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.applicationWindowToken, 0)
+    }
+}
